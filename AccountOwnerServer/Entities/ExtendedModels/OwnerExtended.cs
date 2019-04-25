ca883941334e65037a4a1502c5c8284d8e5f1212
MICHAELSWONG <mizik_jah@yahoo.com>
2019-04-25 11:17:19 -0400

    Finished webapi section

---
+++ AccountOwnerServer/Entities/ExtendedModels/OwnerExtended.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Entities.Models;
+using System.Text;
+
+namespace Entities.ExtendedModels
+{
+    public class OwnerExtended : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+
+        public IEnumerable<Account> Accounts { get; set; }
+
+        public OwnerExtended()
+        {
+
+        }
+
+        public OwnerExtended(Owner owner)
+        {
+            Id = owner.Id;
+            Name = owner.Name;
+            DateOfBirth = owner.DateOfBirth;
+            Address = owner.Address;
+        }
+    }
+}
